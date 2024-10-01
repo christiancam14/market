33d766f8498697648af7447cfb8f1d22c32b4304
Christian Stiven Camacho Galvis <chriscamacho1045@gmail.com>
2024-10-01 03:39:51 -0500

    "Added animations to app component and layout component, updated router links in header component, and added styles for transition panel"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,58 @@
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  NavigationEnd,
+  NavigationStart,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [
+    trigger('panelState', [
+      state(
+        'hidden',
+        style({
+          visibility: 'hidden',
+          transform: 'translateX(100%)',
+        })
+      ),
+      state('visible', style({})),
+      state(
+        'left',
+        style({
+          transform: 'translateX(-100%)',
+        })
+      ),
+      transition('hidden => visible', animate('500ms ease-in-out')),
+      transition('visible => left', animate('500ms ease-in-out')),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'market';
+  panelState = 'hidden';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.panelState = 'visible'; // Mostrar el panel al iniciar la navegación
+
+        setTimeout(() => {
+          this.panelState = 'left'; // Mover el panel a la izquierda después de un tiempo
+        }, 500); // Ajusta el tiempo según la duración de tu animación
+      }
+    });
+  }
 }
--- src/app/shared/ui/layout/layout.component.ts
+++ src/app/shared/ui/layout/layout.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { Router, RouterModule } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-layout',
@@ -9,11 +16,55 @@ import { Router, RouterModule } from '@angular/router';
   imports: [RouterModule, HeaderComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
+  animations: [
+    trigger('panelState', [
+      state(
+        'initial',
+        style({
+          transform: 'translateX(100%)',
+          opacity: 1,
+        })
+      ),
+      state(
+        'hidden',
+        style({
+          transform: 'translateX(100%)',
+          opacity: 1,
+        })
+      ),
+      state(
+        'visible',
+        style({
+          transform: 'translateX(0)',
+          opacity: 1,
+        })
+      ),
+      state(
+        'left',
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 1,
+        })
+      ),
+      transition('initial => visible', animate('500ms linear')),
+      transition('hidden => visible', animate('500ms linear')),
+      transition('visible => left', animate('500ms linear')),
+    ]),
+  ],
 })
 export class LayoutComponent {
   public url: string;
+  public panelState = 'hidden';
   constructor(private router: Router) {
     this.url = router.url;
-    console.log(this.url);
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.panelState = 'visible'; // Mostrar el panel al iniciar la navegación
+
+        setTimeout(() => {
+          this.panelState = 'left'; // Mover el panel a la derecha después de un tiempo
+        }, 500); // Ajusta el tiempo según la duración de tu animación
+      }
+    });
   }
 }
